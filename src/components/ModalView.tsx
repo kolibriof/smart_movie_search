2850e40c1af328a8bae5a7c5d026b64a9bb2a9a3
Dima Savchenko <kolibri380@gmail.com>
2024-01-22 16:52:09 +0100

    enhanced modal visuals

--- src/components/ModalView.tsx
+++ src/components/ModalView.tsx
@@ -3,6 +3,7 @@ import { useAppDispatch, useAppSelector } from "../hooks";
 
 import Modal from "react-modal";
 import { closeModal } from "../slices/FetchMovieSlice";
+import React from "react";
 
 const ModalView: any = () => {
 	const dispatch = useAppDispatch();
@@ -25,41 +26,70 @@ const ModalView: any = () => {
 				isOpen={ModalSettings.opened}
 				className='flex flex-col justify-center items-center bg-white h-fit  translate-x-[50%] translate-y-[50%] rounded-2xl !outline-none shadow-2xl p-5 w-1/2 min-h-[50dvh]'>
 				<div
-					className='absolute right-3 top-2 cursor-pointer bg-red-500 rounded-full p-1 shadow-lg'
+					className='absolute right-3 top-2 cursor-pointer bg-red-500 rounded-full p-1 shadow-lg hover:drop-shadow-xl transition-all duration-150 hover:scale-105 ease-out '
 					onClick={() => dispatch(closeModal())}>
 					<img
 						src='https://img.icons8.com/color/48/cancel--v1.png'
 						alt='close button'
-						className='w-5'
+						className='w-5 '
 					/>
 				</div>
 				{movies[ModalSettings.page || 1]
 					.filter((i) => i.id === ModalSettings.id)
 					.map((i) => {
 						return (
-							<>
+							<section
+								className='flex flex-col justify-center items-center p-5  min-h-[50dvh] '
+								key={i.id}>
 								<div className='flex'>
 									<h3 className='font-bold text-[1.5rem] pb-5'>
 										{i.titleText.text}
 									</h3>
 								</div>
 								<div className='flex justify-center gap-16 items-center flex-row flex-grow w-full'>
-									<div className='flex max-w-[50%]'>
+									<div className='flex max-w-[50%] justify-center'>
 										<img
 											src={i.primaryImage ? i.primaryImage.url : ""}
 											alt={i.titleText.text}
-											className='rounded-lg border-solid border-4 border-black shadow-lg max-w-[50%]'
+											className='rounded-lg border-solid border-4 border-black shadow-lg max-w-[60%]'
 										/>
 									</div>
-									<div className='flex w-1/4  '>
-										<p>
-											{i.plot
-												? i.plot.plotText.plainText
-												: "No plot available."}
-										</p>
+									<div className='flex w-1/2 flex-col'>
+										<div className='flex flex-row gap-2'>
+											<label htmlFor='year' className='font-bold'>
+												Year released:
+											</label>
+											<p id='year'>{i.releaseYear.year}</p>
+										</div>
+										<div className='flex flex-row gap-2 items-center'>
+											<label htmlFor='genre' className='font-bold'>
+												Genres:
+											</label>
+											<p
+												id='genre'
+												className='inline-flex gap-2 text-white font-semibold'>
+												{i.genres.genres.map((i: any) => {
+													return (
+														<p className='bg-slate-400 rounded-md p-1 drop-shadow-lg'>
+															{i.text}
+														</p>
+													);
+												})}
+											</p>
+										</div>
+										<div className='flex flex-row gap-2'>
+											<label htmlFor='plot' className='font-bold'>
+												Plot:
+											</label>
+											<p id='plot'>
+												{i.plot && i.plot.plotText
+													? i.plot.plotText.plainText
+													: "No plot available."}
+											</p>
+										</div>
 									</div>
 								</div>
-							</>
+							</section>
 						);
 					})}
 			</Modal>
